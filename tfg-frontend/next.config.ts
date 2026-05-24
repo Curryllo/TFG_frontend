2afd17c2ea11e1977644e3eab0d6d19148ff0424
Curryllo <842545@unizar.es>
2026-05-24 11:04:18 +0200

    Intento de arreglo de conexion con S3

--- tfg-frontend/next.config.ts
+++ tfg-frontend/next.config.ts
@@ -1,14 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['@aws-sdk/client-s3', '@aws-sdk/s3-presinger'],
+  serverExternalPackages: ['@aws-sdk/client-s3', '@aws-sdk/s3-presigner', '@aws-sdk/lib-storage'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...(config.externals || []), '@aws-sdk/client-s3', '@aws-sdk/s3-presigner']
+    }
+    return config
+  },
   async rewrites() {
     const backendUrl = process.env.BACKEND_URL || 'http://localhost:8080';
     return [
       {
-        // Intercepta cualquier llamada del frontend que empiece por /api/
         source: '/api/:path*',
-        // Y la redirige internamente a tu backend
         destination: `${backendUrl}/api/:path*`,
       },
     ];
